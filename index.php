3148413a74730b535502a3a4ae8b8ac915266210
Mathis FOLLIN <mathis.follin@free.fr>
2025-10-28 18:31:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>FAH Marie-Curie</title>
     <link rel="icon" href="assets/logos/flavicon.png" type="image/x.icon">
     <!-- ... head ... -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
@@ -19,7 +19,37 @@
         <!-- Contenu de la page -->
         <div class="content" data-scroll-section>
             <h1>Contenu de la page</h1>
-        </div>
+            <div class="logos">
+                <div class="logos-slide">
+                    <img scr="assets/logos/1l1s.png" />
+                    <img scr="assets/logos/alfa.png" />
+                    <img scr="assets/logos/caaee.png" />
+                    <img scr="assets/logos/cesacom.png" />
+                    <img scr="assets/logos/emrys.png" />
+                    <img scr="assets/logos/fleau.png" />
+                    <img scr="assets/logos/helloasso.png" />
+                    <img scr="assets/logos/lcl.png" />
+                    <img scr="assets/logos/mc.png" />
+                    <img scr="assets/logos/rapsodie.png" />
+                    <img scr="assets/logos/stj.png" />
+                    <img scr="assets/logos/yapla.png" />
+                </div>
+                <div class="logos-slide">
+                    <img scr="assets/logos/1l1s.png" />
+                    <img scr="assets/logos/alfa.png" />
+                    <img scr="assets/logos/caaee.png" />
+                    <img scr="assets/logos/cesacom.png" />
+                    <img scr="assets/logos/emrys.png" />
+                    <img scr="assets/logos/fleau.png" />
+                    <img scr="assets/logos/helloasso.png" />
+                    <img scr="assets/logos/lcl.png" />
+                    <img scr="assets/logos/mc.png" />
+                    <img scr="assets/logos/rapsodie.png" />
+                    <img scr="assets/logos/stj.png" />
+                    <img scr="assets/logos/yapla.png" />
+                </div>
+            </div> 
+        </div>   
 
         <?php include 'includes/footer.php'; ?>
 
